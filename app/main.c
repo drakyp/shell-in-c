2001b822bc68f97ee0e0f89451b7db99b1025005
William Lam <william.lam@epita.fr>
2024-06-18 11:18:03 +0800

    command not found

--- app/main.c
+++ app/main.c
@@ -2,11 +2,16 @@
 
 int main() {
 
-   printf("$ ");
-   fflush(stdout);
+    printf("$ ");
+    fflush(stdout);
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  return 0;
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
+
+    // take the input add the null character and return command not found
+    input[strlen(input) - 1] = '\0';
+    printf("%s: command not found\n", input);
+
+    return 0;
 }
