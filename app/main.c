267ec0f0f8943dd053f23a4b770fc83918ccd78c
William Lam <william.lam@epita.fr>
2024-06-18 11:44:35 +0800

    while true ?

--- app/main.c
+++ app/main.c
@@ -7,14 +7,14 @@ int main() {
 
     printf("$ ");
     fflush(stdout);
-
+    
+    while(true){
     // Wait for user input
     char input[100];
     fgets(input, 100, stdin);
 
     // take the input add the null character and return command not found
-    input[strlen(input) - 1] = '\0';
-    while (true){
+        input[strlen(input) - 1] = '\0';
 
         printf("%s: command not found\n", input);
     }
