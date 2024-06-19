b6e3bbce63d0882e8fb7b975118b200a65956175
William Lam <william.lam@epita.fr>
2024-06-19 15:10:09 +0800

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -4,23 +4,26 @@
 #include <stdbool.h>
 
 int main() {
-
+    //repl
     while(1){
 
-    printf("$ ");
-    fflush(stdout);
-    
+        printf("$ ");
+        fflush(stdout);
 
-    // Wait for user input
-    char input[100];
-    fgets(input, 100, stdin);
 
-    // take the input add the null character and return command not found
+        // Wait for user input
+        char input[100];
+        fgets(input, 100, stdin);
+
+        // take the input add the null character and return command not found
         input[strlen(input) - 1] = '\0';
+
+        if (!strcmp(input,"exit 0")) //because strcmp return 0 if the input is equal 
+            exit(0);
 
         printf("%s: command not found\n", input);
     }
-    // A REPL
+
 
     return 0;
 }
